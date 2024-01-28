09a49fc4198bce9ac24089803e5106fd4c5a445e
--replace-all <miqayelpetrosyan@gmail.com>
2024-01-28 20:15:45 +0400

    fix user class

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,10 @@
 namespace App\Models;
 
 use DefStudio\Telegraph\Models\TelegraphChat;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class User extends TelegraphChat
 {
-    use HasFactory;
-
     public function data(): HasOne
     {
         return $this->hasOne(UserData::class);
