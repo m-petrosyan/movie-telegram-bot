8bfe9d3f88fd778304f1509d6681b4badc286092
miqay <miqayelpetrosyan@gmail.com>
2024-01-31 00:28:19 +0400

    fix start if game end

--- app/Telegram/TelegramHandler.php
+++ app/Telegram/TelegramHandler.php
@@ -113,7 +113,7 @@ class TelegramHandler extends WebhookHandler
 
     public function getChatId(int $chat_id = null): int
     {
-        return $chat_id ?? $this->message?->from()->id() ?? $this->message->from()->id() ?? $this->data->get('chat_id');
+        return $chat_id ?? $this->message?->from()->id()  ?? $this->data->get('chat_id') ?? $this->message->id();
     }
 
     public function getUser()
